2720a410fa81e099b2b84bad98952be829dfc97a
iilitlleboyii <405422114@qq.com>
2024-03-24 13:31:27 +0800

    🐳 chore: 更新依赖

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,73 +1,7 @@
 <template>
-  <div class="app-container">
-    <div class="flex gap-4">
-      <TreeSelector :dataSource="data" @nodes-change="onNodesChange"></TreeSelector>
-      <div class="h-lg">
-        <FileUpload
-          :mime="['image/jpeg', 'image/png']"
-          :limit="1024 * 500"
-          tip="文件类型仅限jpeg/png，大小不得超过500kb"
-        ></FileUpload>
-      </div>
-    </div>
-  </div>
+  <div class="app-container">首页</div>
 </template>
 
-<script setup name="Home">
-const data = [
-  {
-    id: 1,
-    label: 'Level one 1',
-    children: [
-      {
-        id: 4,
-        label: 'Level two 1-1',
-        children: [
-          {
-            id: 9,
-            label: 'Level three 1-1-1'
-          },
-          {
-            id: 10,
-            label: 'Level three 1-1-2 Level three 1-1-2 Level three 1-1-2'
-          }
-        ]
-      }
-    ]
-  },
-  {
-    id: 2,
-    label: 'Level one 2',
-    children: [
-      {
-        id: 5,
-        label: 'Level two 2-1'
-      },
-      {
-        id: 6,
-        label: 'Level two 2-2'
-      }
-    ]
-  },
-  {
-    id: 3,
-    label: 'Level one 3',
-    children: [
-      {
-        id: 7,
-        label: 'Level two 3-1'
-      },
-      {
-        id: 8,
-        label: 'Level two 3-2'
-      }
-    ]
-  }
-]
-
-function onNodesChange(data) {
-  console.log(data)
-}
-</script>
+<script setup name="Home"></script>
 
 <style lang="scss" scoped></style>
