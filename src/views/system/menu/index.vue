e9f497c9da631c9d54fb49307d0a48f2c7bf1539
iilitlleboyii <405422114@qq.com>
2024-04-23 17:50:27 +0800

    🎈 perf: 尝试markdown-it和shiki

--- src/views/system/menu/index.vue
+++ src/views/system/menu/index.vue
@@ -5,20 +5,52 @@
 </template>
 
 <script setup name="Menu">
-import { codeToHtml } from 'shiki'
+import MarkdownIt from 'markdown-it'
+import { fromHighlighter } from '@shikijs/markdown-it/core'
+import { getHighlighterCore } from 'shiki/core'
+import { codeToTokens } from 'shiki'
+import { bundledLanguages, bundledThemes, getHighlighter } from 'shiki'
+import getWasm from 'shiki/wasm'
+import vues from './vue.md?raw'
+import javas from './java.md?raw'
 
-const code = `<template>
-<div>
-<h1>hello word</h1>  
-</div>  
-</template>
-`
-const html = await codeToHtml(code, {
-  lang: 'vue',
-  theme: 'vitesse-dark'
+// const highlighter = await getHighlighterCore({
+//   themes: [import('shiki/themes/vitesse-light.mjs'), import('shiki/themes/monokai.mjs')],
+//   langs: [
+//     import('shiki/langs/javascript.mjs'),
+//     import('shiki/langs/vue.mjs'),
+//     import('shiki/langs/java.mjs'),
+//     import('shiki/langs/sh.mjs'),
+//     import('shiki/langs/md.mjs')
+//   ],
+//   loadWasm: getWasm
+// })
+
+const highlighter = await getHighlighter({
+  themes: Object.keys(bundledThemes),
+  langs: Object.keys(bundledLanguages)
 })
 
-console.log(html) // 带有高亮显示的 HTML 字符串
+const { tokens } = await codeToTokens('<div class="foo">bar</div>', {
+  lang: 'html',
+  theme: 'min-dark'
+})
+
+console.log(tokens)
+
+const md = MarkdownIt()
+
+md.use(
+  fromHighlighter(highlighter, {
+    themes: {
+      light: 'vitesse-light',
+      dark: 'monokai'
+    },
+    defaultColor: 'light'
+  })
+)
+
+const html = md.render(javas)
 </script>
 
 <style lang="scss" scoped></style>
